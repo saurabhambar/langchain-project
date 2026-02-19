972714e9ba4a9d166ac67af4b28384460c5e72eb
saurabhambar <saurabh.ambar2017@gmail.com>
2026-02-20 00:54:48 +0530

    Custom Prompt and Schemas with Output Parser

--- main.py
+++ main.py
@@ -9,13 +9,28 @@ from langchain_tavily import TavilySearch
 # from langchain_cerebras import ChatCerebras
 from langchain_openai import ChatOpenAI
 
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
+
 tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-4-")
 react_prompt = hub.pull("hwchase17/react")
+
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names"],
+).partial(format_instructions=output_parser.get_format_instructions())
+
+
 agent = create_react_agent(
     llm=llm,
     tools=tools,
-    prompt=react_prompt,
+    prompt=react_prompt_with_format_instructions,
 )
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 chain = agent_executor
